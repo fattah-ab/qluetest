6990f19f3cf9fa5d008b3ca8362ab4081f524e32
fattah-ab <fattaha.12@gmail.com>
2021-01-05 15:24:22 +0700

    push nodeJS

---
+++ nodeJS/nodeJS.js
@@ -0,0 +1,17 @@
+function getPrimes(n) {
+
+    let m = [];
+    nextPrime:
+    for (let i = 2; i < n; i++) { 
+   
+      for (let j = 2; j < i; j++) { 
+        if (i % j == 0) {
+          continue nextPrime;
+        } // not a prime, go next i
+      }
+      m.push(i);
+    }
+    return m;
+  }
+  console.log(getPrimes( 5 )); // a prime
+  console.log(getPrimes( 10 ));
